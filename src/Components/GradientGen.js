5cbfac3f7358475863b09dc94f2420501ed7e109
Ashton Christensen <armadillomaster@gmail.com>
2019-01-31 00:15:15 -0700

    eslint as being wack
    title

--- src/Components/GradientGen.js
+++ src/Components/GradientGen.js
@@ -1,19 +1,19 @@
-import React, { Component } from "react";
-import "./GradientGen.css";
-var ColorScheme = require("color-scheme");
+import React, { Component } from 'react';
+import './GradientGen.css';
+var ColorScheme = require('color-scheme');
 
 export default class GradientGen extends React.Component {
   constructor() {
     super();
     this.state = {
-      gradient: `linear-gradient(to bottom, rgba(255,255,255, .6), rgba(186,186,186,0)),   linear-gradient(to right, #B78A9D, #e380ff)`
+      gradient: `linear-gradient(to bottom, rgba(255,255,255, .6), rgba(186,186,186,0)),   linear-gradient(to right, #B78A9D, #e380ff)`,
     };
     this.getScheme = this.getScheme.bind(this);
   }
 
   getRandomColor() {
-    var letters = "0123456789ABCDEF";
-    var color = "";
+    var letters = '0123456789ABCDEF';
+    var color = '';
     for (var i = 0; i < 6; i++) {
       color += letters[Math.floor(Math.random() * 16)];
     }
@@ -25,15 +25,15 @@ export default class GradientGen extends React.Component {
     let color = this.getRandomColor();
     scheme
       .from_hex(color)
-      .scheme("analogic")
-      .variation("light");
+      .scheme('analogic')
+      .variation('pastel');
     var colors = scheme.colors();
     console.log(colors);
-    //newDiv.style.setProperty("background-image", `linear-gradient(45deg, rgba(255,255,255, .6), rgba(186,186,186,0)), linear-gradient(to right, #${colors[0]}, #${colors[Math.floor(Math.random()*11)]})`)
+    // newDiv.style.setProperty("background-image", `linear-gradient(45deg, rgba(255,255,255, .6), rgba(186,186,186,0)), linear-gradient(to right, #${colors[0]}, #${colors[Math.floor(Math.random()*11)]})`)
     this.setState({
       gradient: `linear-gradient(to bottom, rgba(255,255,255, .6), rgba(186,186,186,0)),   linear-gradient(to right, #${
         colors[0]
-      }, #${colors[Math.floor(Math.random() * 11)]})`
+      }, #${colors[Math.floor(Math.random() * 11)]})`,
     });
     return colors;
   }
